760ffc4a9d45d114bdcf8d98be0ed4b3e968c8a5
karl anderson <karl@2600hz.com>
2013-06-23 21:53:39 -0700

    KAZOO-929: start

---
+++ src/reg_authn_req.erl
@@ -0,0 +1,82 @@
+%%%-------------------------------------------------------------------
+%%% @copyright (C) 2011-2012, VoIP INC
+%%% @doc
+%%% Handle authn_req messages
+%%% @end
+%%% @contributors
+%%%   James Aimonetti
+%%%-------------------------------------------------------------------
+-module(reg_authn_req).
+
+-export([init/0, handle_req/2]).
+
+-include("reg.hrl").
+
+init() ->
+    ok.
+
+-spec handle_req(wh_json:json_object(), proplist()) -> 'ok'.
+handle_req(JObj, _Props) ->
+    true = wapi_authn:req_v(JObj),
+
+    put(callid, wh_json:get_value(<<"Msg-ID">>, JObj, <<"000000000000">>)),
+
+    Username = wapi_authn:get_auth_user(JObj),
+    Realm = wapi_authn:get_auth_realm(JObj),
+
+    lager:debug("trying to authenticate ~s@~s", [Username, Realm]),
+
+    case reg_util:lookup_auth_user(Username, Realm) of
+        {ok, #auth_user{}=AuthUser} ->
+            send_auth_resp(AuthUser, JObj);
+        {error, not_found} ->
+            IPAddress = wh_json:get_value(<<"Orig-IP">>, JObj),
+            lager:notice("auth failure for ~s@~s from ip ~s", [Username, Realm, IPAddress]),
+            send_auth_error(JObj)
+    end.
+
+%%-----------------------------------------------------------------------------
+%% @private
+%% @doc
+%% extract the auth realm from the API request, using the requests to domain
+%% when provided with an IP
+%% @end
+%%-----------------------------------------------------------------------------
+-spec send_auth_resp/2  :: (#auth_user{}, wh_json:json_object()) -> 'ok'.
+send_auth_resp(#auth_user{password=Password, method=Method}=AuthUser, JObj) ->
+    Category = wh_json:get_value(<<"Event-Category">>, JObj),
+    Resp = [{<<"Msg-ID">>, wh_json:get_value(<<"Msg-ID">>, JObj)}
+            ,{<<"Auth-Password">>, Password}
+            ,{<<"Auth-Method">>, Method}
+            ,{<<"Custom-Channel-Vars">>, create_ccvs(AuthUser)}
+            | wh_api:default_headers(Category, <<"authn_resp">>, ?APP_NAME, ?APP_VERSION)
+           ],
+    lager:debug("sending SIP authentication reply, with credentials"),
+    wapi_authn:publish_resp(wh_json:get_value(<<"Server-ID">>, JObj), Resp).
+
+%%-----------------------------------------------------------------------------
+%% @private
+%% @doc
+%% extract the auth realm from the API request, using the requests to domain
+%% when provided with an IP
+%% @end
+%%-----------------------------------------------------------------------------
+-spec send_auth_error(wh_json:json_object()) -> 'ok'.
+send_auth_error(JObj) ->
+    Resp = [{<<"Msg-ID">>, wh_json:get_value(<<"Msg-ID">>, JObj)}
+            | wh_api:default_headers(?APP_NAME, ?APP_VERSION)
+           ],
+    lager:debug("sending SIP authentication error"),
+    wapi_authn:publish_error(wh_json:get_value(<<"Server-ID">>, JObj), Resp).
+
+-spec create_ccvs(#auth_user{}) -> wh_json:object().
+create_ccvs(#auth_user{}=AuthUser) ->    
+    Props = [{<<"Username">>, AuthUser#auth_user.username}
+             ,{<<"Realm">>, AuthUser#auth_user.realm}
+             ,{<<"Account-ID">>, AuthUser#auth_user.account_id}
+             ,{<<"Authorizing-ID">>, AuthUser#auth_user.authorizing_id}
+             ,{<<"Authorizing-Type">>, AuthUser#auth_user.authorizing_type}
+             ,{<<"Owner-ID">>, AuthUser#auth_user.owner_id}
+             ,{<<"Inception">>, <<"on-net">>}
+            ],
+    wh_json:from_list(props:filter_undefined(Props)).
---
+++ src/reg_route_req.erl
@@ -0,0 +1,62 @@
+%%%-------------------------------------------------------------------
+%%% @copyright (C) 2012, VoIP INC
+%%% @doc
+%%% Look up IP for authorization/replaying of route_req
+%%% @end
+%%% @contributors
+%%%   James Aimonetti
+%%%-------------------------------------------------------------------
+-module(reg_route_req).
+
+-export([init/0, handle_route_req/2]).
+
+-include("reg.hrl").
+
+init() -> whapps_maintenance:refresh(?WH_SIP_DB).
+
+-spec handle_route_req(wh_json:json_object(), wh_proplist()) -> any().
+handle_route_req(JObj, _Props) ->
+    true = wapi_route:req_v(JObj),
+    maybe_replay_route_req(JObj, wh_json:get_value(<<"From-Network-Addr">>, JObj)).
+
+-spec maybe_replay_route_req(wh_json:json_object(), api_binary()) -> any().
+maybe_replay_route_req(_JObj, undefined) -> ok;
+maybe_replay_route_req(JObj, IP) ->
+    lager:debug("trying to see if this route req is an auth-by-ip'd device: ~s", [IP]),
+
+    CCVs = wh_json:get_value(<<"Custom-Channel-Vars">>, JObj, wh_json:new()),
+
+    maybe_replay_route_req(JObj, IP, wh_json:get_ne_value(<<"Account-ID">>, CCVs), CCVs).
+
+maybe_replay_route_req(JObj, IP, undefined, CCVs) ->
+    case lookup_ip(IP) of
+        {ok, []} ->
+            lager:debug("no entry in ~s for IP ~s", [?WH_SIP_DB, IP]);
+        {ok, [Doc|_]} ->
+            AccountID = wh_json:get_value([<<"value">>,  <<"account_id">>], Doc),
+            OwnerID = wh_json:get_value([<<"value">>, <<"owner_id">>], Doc),
+            AuthType = wh_json:get_value([<<"value">>, <<"authorizing_type">>], Doc, <<"anonymous">>),
+
+            CCVs1 = wh_json:set_values(
+                      props:filter_undefined(
+                        [{<<"Account-ID">>, AccountID}
+                         ,{<<"Owner-ID">>, OwnerID}
+                         ,{<<"Authorizing-ID">>, wh_json:get_value(<<"id">>, Doc)}
+                         ,{<<"Inception">>, <<"on-net">>}
+                         ,{<<"Authorizing-Type">>, AuthType}
+                        ])
+                      ,CCVs),
+
+            lager:debug("adding account ~s and owner ~s to ccvs", [AccountID, OwnerID]),
+
+            JObj1 = wh_json:set_value(<<"Custom-Channel-Vars">>, CCVs1, JObj),
+            lager:debug("replaying route_req"),
+            wapi_route:publish_req(JObj1);
+        {error, _E} ->
+            lager:debug("failed to lookup by ip: ~s: ~p", [IP, _E])
+    end;
+maybe_replay_route_req(_JObj, _IP, _AcctId, _CCVs) ->
+    ok.
+
+lookup_ip(IP) ->
+    couch_mgr:get_results(?WH_SIP_DB, <<"credentials/lookup_by_ip">>, [{<<"key">>, IP}]).
